69e67318f1947227c5d6a7a139c50edc5868df8c
walidmahade <walidmahade@gmail.com>
2024-08-07 04:36:53 +0600

    Auto commit

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 import './style.css';
 import { setup } from './stars.ts';
-import { initHeaderAnimation } from './header.ts';
+// import { initHeaderAnimation } from './header.ts';
 import { solarSystemAnimation } from './solarSystemAnimation.ts';
 
 // on page load call setup function
 window.onload = () => {
     setup();
     // gsap header animation
-    initHeaderAnimation();
+    // initHeaderAnimation();
     // gsap solar system animation
     solarSystemAnimation();
 };
